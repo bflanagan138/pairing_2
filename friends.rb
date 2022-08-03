85a460a80aa77ba5145f615706dd33130d0c258e
Bryan Flanagan <bflanagan78@yahoo.com>
2022-08-03 14:22:00 -0600

    Finish Exercise 2

--- friends.rb
+++ friends.rb
@@ -1,6 +1,10 @@
-bf = "John"
-if bf == "Chris"
+bf = "Mike"
+not_bf = "Dave"
+if bf == "Chris" && not_bf == "John"
   puts "Long time no see!"
+  puts "John, good to see you too!"
+elsif bf != "Chris" && not_bf == "John"
+  puts "John, good to see you, where is Chris?"
 else
   puts "Who are you?!"
 end
